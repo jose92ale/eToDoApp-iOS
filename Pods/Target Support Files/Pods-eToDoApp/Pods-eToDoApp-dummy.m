583e09937bf9ad65612739cef9d70f8ac0eea73d
jose92ale <jose92ale@gmail.com>
2017-02-20 21:20:13 -0500

    First commit

---
+++ Pods/Target Support Files/Pods-eToDoApp/Pods-eToDoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eToDoApp : NSObject
+@end
+@implementation PodsDummy_Pods_eToDoApp
+@end
---
+++ Pods/Target Support Files/Pods-eToDoApp/Pods-eToDoApp-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_eToDoAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_eToDoAppVersionString[];
+
